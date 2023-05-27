ac4b7eff19bd30742e4feefc191680cb1faf8dc5
Ashendhiyan <ashen13516@gmail.com>
2023-05-26 20:58:34 -0800

    2nd testing

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+//scroll section
+let section =  document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height){
+         //active nabar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+}
